ddba93ca8f01e37bc9f0f3171ad50675f9eab72b
Sulkhan <suxozoidze2004@gmail.com>
2025-04-02 15:35:42 +0400

    User icon change

--- frontend/src/components/UserIcon.tsx
+++ frontend/src/components/UserIcon.tsx
@@ -2,7 +2,8 @@ import { Status } from "../types/types";
 
 type Props = {
   image: string;
-  status: Status;
+  status?: Status;
+  compact?: boolean;
 };
 
 const colors = {
@@ -11,17 +12,23 @@ const colors = {
   away: "bg-amber-500",
 };
 
-const UserIcon = ({ image, status }: Props) => {
+const UserIcon = ({ image, status, compact }: Props) => {
   return (
     <div className="relative">
       <img
         draggable={false}
         src={"https://avatar.iran.liara.run/public" + image}
-        className="size-12 min-w-12 rounded-full peer"
+        className={`${
+          compact ? "size-10 min-w-10" : "size-12 min-w-12"
+        } rounded-full select-none peer`}
       />
-      <span
-        className={`${colors[status]} content-[''] size-4 absolute rounded-full bottom-0 right-0 border-2 border-light-card dark:border-dark-card transition-colors`}
-      />
+      {status && (
+        <span
+          className={`${colors[status]} ${
+            compact ? "size-3.5" : "size-4"
+          } content-[''] absolute rounded-full bottom-0 right-0 border-2 border-light-card dark:border-dark-card transition-colors`}
+        />
+      )}
     </div>
   );
 };
