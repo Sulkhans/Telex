325638338af105f79ddadaa76049000498550688
Sulkhan <suxozoidze2004@gmail.com>
2025-05-04 17:16:20 +0400

    ~

--- frontend/src/components/Skeleton.tsx
+++ frontend/src/components/Skeleton.tsx
@@ -1,11 +1,16 @@
 type Props = {
   quantity: number;
+  compact?: boolean;
 };
 
-const Skeleton = ({ quantity }: Props) => {
+const Skeleton = ({ quantity, compact = false }: Props) => {
   return new Array(quantity).fill(0).map((_, index) => (
     <div key={index} className="p-2 flex items-center gap-4">
-      <div className="size-12 rounded-full bg-primary/10 dark:bg-foreground/10 animate-pulse" />
+      <div
+        className={`${
+          compact ? "size-10" : "size-12"
+        } rounded-full bg-primary/10 dark:bg-foreground/10 animate-pulse`}
+      />
       <div className="space-y-2">
         <div className="w-36 h-4 rounded-md bg-primary/10 dark:bg-foreground/10 animate-pulse" />
         <div className="w-20 h-4 rounded-md bg-primary/10 dark:bg-foreground/10 animate-pulse" />
