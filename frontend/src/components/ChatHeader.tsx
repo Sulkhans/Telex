8fa4319baa43174c2e551b3803dffd77e6b39d24
Sulkhan <suxozoidze2004@gmail.com>
2025-05-06 22:45:22 +0400

    Remove friend

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import { useChat } from "../context/ChatContext";
+import { deleteFriend } from "../api/friends";
 import UserIcon from "./UserIcon";
 import Options from "../assets/options.svg?react";
+import Modal from "./Modal";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 const ChatHeader = () => {
   const { selected, toggleDetails } = useChat();
+  const [showModal, setShowModal] = useState(false);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: removeFriend } = useMutation({
+    mutationFn: (id: string) => deleteFriend(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["friend"] });
+      navigate("/chat");
+    },
+  });
 
   if (!selected) return null;
   const isFriend = selected.type === "friend";
 
-  const handleClick = () => (isFriend ? null : toggleDetails());
+  const handleClick = () => (isFriend ? setShowModal(true) : toggleDetails());
 
   return (
-    <header className="flex items-center px-4 py-3 select-none">
+    <header className="flex items-center px-4 py-3 relative select-none">
       <div className="flex items-center gap-4">
         {isFriend ? (
           <UserIcon
@@ -41,6 +58,28 @@ const ChatHeader = () => {
       >
         <Options className="mx-auto size-5 stroke-primary dark:stroke-foreground transition-colors" />
       </button>
+      {isFriend && (
+        <Modal
+          title="Remove Friend"
+          isOpen={showModal}
+          onClose={() => setShowModal(false)}
+        >
+          <p className="-mt-2 mb-2 text-sm text-secondary">
+            Are you sure you want to unfriend {selected.data.username}?
+          </p>
+          <div className="flex justify-end gap-2 text-sm font-medium">
+            <button onClick={() => setShowModal(false)} className="px-4">
+              Cancel
+            </button>
+            <button
+              onClick={() => removeFriend(selected.data.friendshipId)}
+              className="px-4 py-2 bg-red-600 hover:bg-red-600/80 text-white rounded-md"
+            >
+              Confirm
+            </button>
+          </div>
+        </Modal>
+      )}
     </header>
   );
 };
